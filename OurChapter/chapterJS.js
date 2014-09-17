82e623096971a6961721a6ee8fa02239e6d20cb2
Braden Shill <shill.braden@gmail.com>
2014-09-17 17:15:34 -0600

    Appended Comments
    Cleaned the code a little too.

--- OurChapter/chapterJS.js
+++ OurChapter/chapterJS.js
@@ -2,36 +2,36 @@ angular.module('App')
 	.controller('chapterController', function($scope,$interval){
 		console.log('controller: chapterController');	
 		
-		var topOffset = 100;
+		var topOffset = 100; //this needs to be changed if we change the height of the menuBar
+		var scrollSpeed = 1000;// adjusts the auto-scrolling speed
 
-		$("#leadershipBTN").click(function () {
+		
+		//Auto Scrolling
+		$("#leadershipBTN").click(function () { 
 	        $('html, body').animate({
 	        	scrollTop: $("#leadership").offset().top - topOffset
-	        }, 1000);
+	        }, scrollSpeed);
     	});
     	$("#eventsBTN").click(function () {
 	        $('html, body').animate({
 	        	scrollTop: $("#eventScreen").offset().top - topOffset
-	        }, 1000);
+	        }, scrollSpeed);
     	});
     	$("#administrationBTN").click(function () {
 	        $('html, body').animate({
 	        	scrollTop: $("#administration").offset().top - topOffset
-	        }, 1000);
+	        }, scrollSpeed);
     	});
     	$("#timelineBTN").click(function () {
 	        $('html, body').animate({
 	        	scrollTop: $("#timeline").offset().top - topOffset
-	        }, 1000);
+	        }, scrollSpeed);
     	});
     	$("#topBTN").click(function () {
 	        $('html, body').animate({
 	        	scrollTop: $("#top").offset().top - topOffset
-	        }, 1000);
+	        }, scrollSpeed);
     	});
+
     	$(".screen").css({"height" : $( window ).height()-120 +'px'});
-    	
-    	$interval(function(){
-    		$scope.teamImg = 'http://nuamestsa.com/TSA/imgs/black_linen_v2.png';
-    	},5000)
 	});
