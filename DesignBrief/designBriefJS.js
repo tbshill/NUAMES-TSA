4bad0769cbdeb69c6fa68541dddb55506488388e
tobinegbert <tobinegbert@gmail.com>
2014-09-24 20:53:56 -0600

    Design Brief Interval

--- DesignBrief/designBriefJS.js
+++ DesignBrief/designBriefJS.js
@@ -8,11 +8,40 @@ angular.module('App').controller('designBrief', ['$scope', '$interval',
         $scope.start = function() {
                 if (angular.isDefined(stop)) return;
 
+            var alertOn = true;
+
             stop = $interval(function(){
+
                 if($scope.design_brief >= 0){
-                    $scope.design_brief = $scope.design_brief + 37
+                    $scope.design_brief = $scope.design_brief + 17;
+
+                    if (alertOn && $scope.design_brief >= 0 && $scope.design_brief <= 99) {
+                        alertOn = false;
+                        $(document).ready(function(){
+                            $('h3').text('Stuff')
+                        })
+                    } alertOn = true;
+                    if (alertOn && $scope.design_brief >= 100 && $scope.design_brief <= 199) {
+                        $(document).ready(function () {
+                            $('h3').css('color', 'white')
+                        });
+                    }
+                        alertOn = true;
+                    if(alertOn && $scope.design_brief >= 200 && $scope.design_brief <= 299){
+                        $(document).ready(function(){
+                            $('h3').text('We will change the background here')
+                        });
+                        alertOn = false;
+                    }
+                    alertOn = true;
+                    if(alertOn && $scope.design_brief >= 300 && $scope.design_brief <= 399) {
+                        $(document).ready(function(){
+                            $('h3').fadeIn().css('color', 'purple')
+                        })
+                    }
+
                 }
-            }, 1000)
+            }, 150)
         };
         $scope.stopFight = function() {
             if (angular.isDefined(stop)) {
