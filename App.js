7d8273c007e4fd9d17742310ee30af73e75d66e6
Braden Shill <shill.braden@gmail.com>
2014-09-25 18:37:05 -0600

    Parallax Group Photo on Chapter Page

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-var app = angular.module('App',['ui.router','ngRoute','firebase']) //Define the angular app
+var app = angular.module('App',['ui.router','ngRoute','firebase','duParallax']) //Define the angular app
 	.controller('mainController',function($rootScope, $firebase,$scope,$location){ //mainController is referenced in index.html
 		$rootScope.eventList = [ //just a global list I declare immediately.
 			'Animatronics',
