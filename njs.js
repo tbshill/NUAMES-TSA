4938c666f35f4ed52a5bf326b0288649b134b250
tobinegbert <tobinegbert@gmail.com>
2014-09-25 19:57:37 -0600

    Live chat in the console

--- njs.js
+++ njs.js
@@ -1,11 +1,29 @@
-var express = express(),
-    app = require('express')(),
+//var express = require('express'),
+//    app = express(),
+//    io = require('socket.io')(http),
+//    MongoClient = require('mongodb').MongoClient,
+//    mongoose = require('mongoose'),
+//    htmlRoot = '/Users/tobinegbert/WebstormProjects/notes-app/',
+//    path = require('path'),
+//    app2 = express.createServer();
+//    socket = socket.listen(app2);
+
+var app = require('express')(),
     http = require('http').Server(app),
-    io = require('socket.io')(http),
-    MongoClient = require('mongodb').MongoClient,
-    mongoose = require('mongoose'),
-    Cookies = require( "cookies"),
-    path = require('path');
+    io = require('socket.io')(http);
+
+
+io.on('connection', function(socket){
+    console.log('a user connected');
+    socket.on('disconnect', function(){
+        console.log('user disconnected');
+    });
+});
+io.on('connection', function(socket){
+    socket.on('chat message', function(msg){
+        console.log('message: ' + msg);
+    });
+});
 
 app.get('/', function(req, res){
     res.sendFile(__dirname + '/index.html');
@@ -16,9 +34,6 @@ app.get('/CSS/*', function(req, res){
 app.get('/*', function(req,res){
     res.sendFile(__dirname + req.path);
 });
-//app.get('/socket.io', function(req, res){
-//   res.sendFile(__dirname + '/node_modules/socket.io/index.html')
-//});
 app.get('/OurChapter/*', function(req, res){
     res.sendFile(__dirname + req.path);
 });
@@ -38,10 +53,13 @@ app.get('/Sponsers/*', function(req,res){
     res.sendFile(__dirname + req.path);
 });
 
-io.on('connection', function(socket){
-    console.log('Hi');
-});
+//socket.io.on('connection', function(client){
+//    console.log("Someone connected");
+//    socket.on('disconnect', function(){
+//        console.log("Someone disconnected")
+//    });
+//});
 
-app.listen(5678, function(){
-    console.log('Listening on port :5678')
+http.listen(5678, function(){
+    console.log('listening on *:3000');
 });
