f421fcd0f701bc2af5553c6fa506d6938164f147
tobinegbert <tobinegbert@gmail.com>
2014-09-25 20:01:48 -0600

    Live chat. It logs it to the console

--- njs.js
+++ njs.js
@@ -19,6 +19,7 @@ io.on('connection', function(socket){
         console.log('user disconnected');
     });
 });
+
 io.on('connection', function(socket){
     socket.on('chat message', function(msg){
         console.log('message: ' + msg);
