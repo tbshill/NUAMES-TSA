a5a7088fb4b352260c952847479542e339ab4dfd
Braden Shill <shill.braden@gmail.com>
2014-09-28 15:54:15 -0600

    Create posts from Officer Page

--- Manager/Officer/newsFeedJS.js
+++ Manager/Officer/newsFeedJS.js
@@ -1,4 +1,11 @@
 angular.module('App')
-	.controller('newsFeedController',function($scope){
+	.controller('newsFeedController',function($scope,$firebase,toaster){
 		console.log('newFeedController');
+
+		$scope.schoolEvents = $firebase(new Firebase('https://nuames-tsa.firebaseio.com/SchoolEvents/')).$asArray();
+		$scope.schoolPost = function(){
+			toaster.pop('success',"Posted","")
+			$scope.schoolEvents.$add($scope.schoolEvent);
+			$scope.schoolEvent = {};
+		}
 	})
--- OurSchool/schoolJS.js
+++ OurSchool/schoolJS.js
@@ -1,57 +1,36 @@
 angular.module('App')
-	.controller('schoolController',function($scope){
+	.controller('schoolController',function($scope,$firebase,toaster){
 		console.log('controller: schoolController');
 			$(".flexBox").click(function(){  //Debug
     		console.log('click');
+    		
     	});
-		$scope.events = [ //From Firebase
-			{
-				img: 'http://lorempixel.com/300/500/city/9', //lorempixel is like loremipsum text but for images
-				title: 'This is Event 1',
-				blurb: 'Look at my little blurb about this photo. Try hovering!'
-			},
-			{
-				img: 'http://lorempixel.com/300/500/city/9',
-				title: 'Test Event 2',
-				blurb: 'this is a filler for the real events'
-			},
-			{
-				img: 'http://lorempixel.com/300/500/city/9',
-				title: 'Test Event 3',
-				blurb: 'this is a filler for the real events'
-			},
-			{
-				img: 'http://lorempixel.com/300/500/city/9',
-				title: 'Test Event 4',
-				blurb: 'this is a filler for the real events'
-			},
-			{
-				img: 'http://lorempixel.com/300/500/city/9',
-				title: 'Test Event 5',
-				blurb: 'this is a filler for the real events'
-			},
-			{
-				img: 'http://lorempixel.com/300/500/city/9',
-				title: 'Test Event 6',
-				blurb: 'this is a filler for the real events'
-			},
-			{
-				img: 'http://lorempixel.com/300/500/city/9',
-				title: 'Test Event 7',
-				blurb: 'this is a filler for the real events'
-			},
-			{
-				img: 'http://lorempixel.com/300/500/city/9',
-				title: 'Test Event 8',
-				blurb: 'this is a filler for the real events'
-			},
-		];
-		$('.og-box').click(function(){
-			$('.og-box').css('height','200px');
-			$('.og-box').find('div').hide();
-			$(this).find('div').show();
-			$(this).css('height','400px');
-		})
-});
-	//This directive will be replaced with this JS: http://tympanus.net/Tutorials/ThumbnailGridExpandingPreview/
 
+			$scope.schoolEvents = $firebase(new Firebase('https://nuames-tsa.firebaseio.com/SchoolEvents/')).$asArray();
+		
+})
+.directive('myPreivew',function(){
+	// Runs during compile
+	return {
+		// name: '',
+		// priority: 1,
+		// terminal: true,
+		// scope: {}, // {} = isolate, true = child, false/undefined = no change
+		// controller: function($scope, $element, $attrs, $transclude) {},
+		// require: 'ngModel', // Array = multiple requires, ? = optional, ^ = check parent elements
+		// restrict: 'A', // E = Element, A = Attribute, C = Class, M = Comment
+		// template: '',
+		// templateUrl: '',
+		// replace: true,
+		// transclude: true,
+		// compile: function(tElement, tAttrs, function transclude(function(scope, cloneLinkingFn){ return function linking(scope, elm, attrs){}})),
+		link: function($scope, iElem, iAttrs, controller) {
+			$(iElem).click(function(){
+				$('.og-box').find('div').hide();
+				$('.og-box').css('height',"200px");
+				$(iElem).find('div').show();
+				$(iElem).css('height',"400px");
+        	});
+		}
+	};
+});  
