60295988880f2babbd8048193fe6cb889f7e10f8
Braden Shill <shill.braden@gmail.com>
2015-02-27 12:36:51 -0700

    Login to email and password

--- Login/LoginJS.js
+++ Login/LoginJS.js
@@ -5,80 +5,64 @@ angular.module('App')
 		var members = $firebase(new Firebase('https://nuames-tsa.firebaseio.com/Members')).$asArray();
 		var newMembers = $firebase(new Firebase('https://nuames-tsa.firebaseio.com/newUsers')).$asArray();
 		
-		var addUser = function(user){
-			user.$bindTo($rootScope,'user');
-			members.$add(user);
-			newMembers.$add({id:user.id, name:user.displayName});
-		};
-
-		/*
-			Firebase supplies a "simple login" which allows users to login with Facebook, Twitter, Google, Github or Username and Password.
-			We are only using Facebook and Google.
-		*/
-
-
-		//GOOGLE Login
-		//See Facebook Login for more detailed explanation
-		$scope.loginWithFacebook = function(){
-			console.log(document.cookie);
-			console.log(members.length);
-
+		$scope.createUser = function(){
+			
+			console.log("HelloWorld");
 			var ref = new Firebase("https://nuames-tsa.firebaseio.com");
-			ref.authWithOAuthPopup("facebook", function(error, authData) {
+			ref.createUser({
+			  email    : $scope.email,
+			  password : $scope.password
+			}, function(error, userData) {
+			  if (error) {
+			    console.log("Error creating user:", error);
+			  } else {
+			    console.log("Successfully created user account with uid:", userData);
+				ref.authWithPassword({
+				  email    : $scope.email,
+				  password : $scope.password
+				}, function(error, authData) {
+				  if (error) {
+				    console.log("Login Failed!", error);
+				  } else {
+				    console.log("Authenticated successfully with payload:", authData);
+				    newUser = {
+				    	"email":authData.password.email,
+				    	"uid":authData.uid,
+				    	"displayName":$scope.name
+				    }
+				    members.$add(newUser);
+				    members.$save();
+				  }
+				});
+
+
+				members.$add(userData.uid);
+				members.$save();
+			  }
+			});
+		}
+		$scope.loginUser = function(){
+			var ref = new Firebase("https://nuames-tsa.firebaseio.com");
+			ref.authWithPassword({
+			  email    : $scope.email,
+			  password : $scope.password
+			}, function(error, authData) {
 			  if (error) {
 			    console.log("Login Failed!", error);
 			  } else {
 			    console.log("Authenticated successfully with payload:", authData);
-			    console.log(authData.facebook.email);
-			    for(i = 0; i< members.length; i++){
-			    	if(members[i].id == authData.facebook.id){
-			    		console.log("Heya--you're all logged in!");
-			    		var u = $firebase(new Firebase('https://nuames-tsa.firebaseio.com/Members/'+members[i].$id)).$asObject(); 
+			     for(i = 0; i<members.length; i++){
+			     	if(authData.uid == members[i].uid){
+			     		var u = $firebase(new Firebase('https://nuames-tsa.firebaseio.com/Members/'+members[i].$id)).$asObject(); 
 		    			u.$bindTo($rootScope,"user"); //3-way data binding
 		    			$location.path("/manager"); //change location
 		    			$rootScope.isLoggedIn = true;
 		    			return; //kill loop and function
-			    	}
-			    }
+			     	}
+			     }
 			  }
-			},{scope:"email"});
-		};
-		$scope.loginWithGoogle = function(){
-			console.log(document.cookie);
-			console.log(members.length);
-			var ref = new Firebase("https://nuames-tsa.firebaseio.com");
-			ref.authWithOAuthPopup("google", function(error, authData) {
-			  if (error) {
-			    console.log("Login Failed!", error);
-			  } else {
-			    console.log("Authenticated successfully with payload:", authData);
-			    console.log(authData.google.email);
-			    for(i = 0; i < members.length; i++) {
-			    	if(members[i].id == authData.google.id) {
-			    		console.log("Heya--you're all logged in!");
-			    		var u = $firebase(new Firebase('https://nuames-tsa.firebaseio.com/Members/'+members[i].$id)).$asObject(); 
-		    			u.$bindTo($rootScope,"user"); //3-way data binding
-		    			$location.path("/manager"); //change location
-		    			$rootScope.isLoggedIn = true;
-		    			return; //kill loop and function
-			    	}
-			    }
-			    console.log("done looking.");
-			  }
-			}, {scope:"email"});
-		};
-
-		var verifyUser = function (id) {
-			for (var i = members.length - 1; i >= 0; i--) {
-	    		if(members[i].id == user.id){
-    				var k = $firebase(new Firebase('https://nuames-tsa.firebaseio.com/Members/'+members[i].$id)).$asObject();
-    				k.member = true;
-    				k.$save();
-    				break;
-    			}
-	    	}
-		};
-		
+			});
+		}
 		var declineUser = function(user){ //Removes User from firebase
 			Members.$remove(user);
 		}
